fbd666f61a0238942c9f684b1f14aae8aac0b63d
Kabir Oberai <oberai.kabir@gmail.com>
2023-07-19 21:16:47 -0400

    Account for SwiftPM updates

--- README.md
+++ README.md
@@ -5,12 +5,15 @@ SDKs for cross-compiling Darwin code on Linux.
 ## Building
 
 Prerequisites:
-- macOS or Linux
-- [jq](https://github.com/jqlang/jq) (`brew install jq` or `apt install jq`)
+- macOS
+    - Xcode
+- Linux
+    - [rsync](https://rsync.samba.org)
+    - [unxip](https://github.com/saagarjha/unxip)
 
 Steps:
-1. Download [Xcode](https://developer.apple.com/download/all/?q=Xcode) 15.0 or higher.
-    - If you're on Linux, you can unpack Xcode.xip using [unxip](https://github.com/saagarjha/unxip).
+1. Download and install/extract [Xcode](https://developer.apple.com/download/all/?q=Xcode) 15.0 or higher
+    - Use `unxip` to extract Xcode if you're on Linux.
 2. Run `./build.sh <linux host> [developer dir]`.
     - **linux host**: You can pass any Linux host for which a darwin-tools-linux [release](https://github.com/kabiroberai/darwin-tools-linux/releases) exists. e.g. `ubuntu22.04-aarch64`.
     - **developer dir**: this should be the path to `Xcode.app/Contents/Developer`. On macOS, you can omit this argument to let the script infer it.
@@ -22,18 +25,18 @@ Find the output at `output/*.artifactbundle`.
 Prerequisites:
 - Linux
 - Swift 5.9 toolchain (<https://swift.org/download>)
+- A built copy of `darwin.artifactbundle` (see **Building**)
 
-Steps:
-1. Build a toolchain as described above.
-2. Run `output/*.artifactbundle/install.sh` on your Linux machine.
+```
+swift experimental-sdk install path/to/darwin.artifactbundle
+```
 
 ## Usage
 
 ```
-swift build --experimental-swift-sdk ios16.0
+swift build --experimental-swift-sdk ios
 ```
 
 ## TODO
 
 - [ ] Make it easy to use CI (nb: we avoid distributing pre-packaged toolchains for legal reasons)
-- [ ] Remove installation script once SwiftPM bugs are fixed
--- build.sh
+++ build.sh
@@ -21,41 +21,23 @@ fi
 
 mkdir -p output
 
+echo "Making base..."
 bundle="output/darwin.artifactbundle"
 rm -rf "$bundle"
 cp -a layout "$bundle"
 
-mkdir -p "$bundle/res"
-cp -a "$dev_dir/Toolchains/XcodeDefault.xctoolchain/usr/lib/"{swift,swift_static,clang} "$bundle/res/"
-
+echo "Installing toolset..."
 mkdir -p "$bundle/toolset"
-curl -#L "https://github.com/kabiroberai/darwin-tools-linux/releases/download/v${DARWIN_TOOLS_VERSION}/darwin-tools-${linux_version}.tar.gz" | tar xvzf - -C "$bundle/toolset" --strip-components=2
+curl -#L "https://github.com/kabiroberai/darwin-tools-linux/releases/download/v${DARWIN_TOOLS_VERSION}/darwin-tools-${linux_version}.tar.gz" \
+    | tar xzf - -C "$bundle/toolset" --strip-components=2
 cp -a templates/dsymutil "$bundle/toolset/bin/"
 
-function add_ver {
-    target="$1$2"
-    mkdir -p "$bundle/targets/$target"
-    sed 's/$TARGET/'$target'/g' templates/toolset-target.json > "$bundle/targets/$target/toolset.json"
-    sed 's/$SDK/'$sdk_name'/g' "templates/swift-sdk.json" > "$bundle/targets/$target/swift-sdk.json"
-    jq '. * $next' --argjson next "$(sed 's/$TARGET/'"$target"'/g' templates/info-base.json)" < "$bundle/info.json" > "$bundle/info.json.tmp"
-    mv "$bundle/info.json"{.tmp,}
-}
+echo "Installing Developer directories..."
+mkdir -p "$bundle/Developer"
+rsync -aW --relative \
+    "$dev_dir/./"Toolchains/XcodeDefault.xctoolchain/usr/lib/{swift,swift_static,clang} \
+    "$dev_dir/./"Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk \
+    "$dev_dir/./"Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk \
+    "$bundle/Developer/"
 
-function add_plat {
-    sdk_path="$dev_dir/Platforms/$2.platform/Developer/SDKs/$2.sdk"
-    sdk_name="$(basename "$sdk_path")"
-    sys="$(jq -r ".SupportedTargets.$1.LLVMTargetTripleSys" < "$sdk_path/SDKSettings.json")"
-    mkdir -p "$bundle/sdks"
-    cp -a "${sdk_path}" "$bundle/sdks/"
-    versions=($(jq -r '.SupportedTargets.'"$1"'.ValidDeploymentTargets | join(" ")' < "$sdk_path/SDKSettings.json"))
-    for version in "${versions[@]}"; do
-        add_ver $sys $version
-        if echo $version | grep -q '^[0-9]*\.0'; then
-            major="$(echo $version | grep -o '^[0-9]*')"
-            add_ver $sys $major
-        fi
-    done
-}
-
-add_plat iphoneos iPhoneOS
-add_plat macosx MacOSX
+echo "Done!"
